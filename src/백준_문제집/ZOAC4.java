21b050fe5850054347d4c80d0547bb156b24f883
dpfprtus <dpfprtus@gmail.com>
2025-01-06 11:00:40 +0900

    정답

--- src/백준_문제집/ZOAC4.java
+++ src/백준_문제집/ZOAC4.java
@@ -13,7 +13,7 @@ public class ZOAC4 {
         int M = in.nextInt();
 
         //첫 번째 요소에 대해서는 거리두기 규칙을 적용할 필요가 없다.
-        int a = (H-1)/(N+1)+1;
+        int a =     (H-1)/(N+1)+1;
         int b = (W-1)/(M+1)+1;
 
         System.out.println(a*b);
