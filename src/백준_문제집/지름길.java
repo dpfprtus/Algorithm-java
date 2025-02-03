58d3f00fc1bcb403e65d60436a70ee65ceb99312
dpfprtus <dpfprtus@gmail.com>
2025-02-03 15:26:29 +0900

    정답(다익스트라)

--- src/백준_문제집/지름길.java
+++ src/백준_문제집/지름길.java
@@ -3,9 +3,17 @@ package 백준_문제집;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
+import org.w3c.dom.Node;
+
 public class 지름길 {
+	static List<List<Node>> list = new ArrayList<>();
+	static int D;
+	static int[] distance;
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,33 +21,50 @@ public class 지름길 {
 		String[] info = br.readLine().split(" ");
 
 		int N = Integer.parseInt(info[0]);
-		int D = Integer.parseInt(info[1]);
+		D = Integer.parseInt(info[1]);
 
 		int[][] arr = new int[N][3];
-
+		for (int i = 0; i <=10001; i++) {
+			list.add(new ArrayList<>());
+		}
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < 3; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
+			list.get(arr[i][0]).add(new Node(arr[i][1], arr[i][2]));
 		}
 
-		int[] dp = new int[D + 1];
+		distance = new int[10001];
 		for (int i = 1; i <= D; i++) {
-			dp[i] = i;
+			distance[i] = i;
 		}
 
-		for (int i = 1; i <= D; i++) {
+		for (int i = 0; i <= D; i++) {
+			dijkstra(i);
+		}
+		System.out.print(distance[D]);
 
-			dp[i] = Math.min(dp[i], dp[i - 1] + 1);
+	}
 
-			for (int j = 0; j < N; j++) {
-				if (i == arr[j][1]) {
-					dp[i] = Math.min(dp[i], dp[arr[j][0]] + arr[j][2]);
-				}
+	public static void dijkstra(int start) {
+		if(start > D) return;
+		if(distance[start+1] > distance[start]+1) distance[start+1] = distance[start]+1;
+
+		for (int i = 0; i < list.get(start).size(); i++) {
+			if (distance[list.get(start).get(i).node] > distance[start] + list.get(start).get(i).weight) {
+				distance[list.get(start).get(i).node] = distance[start] + list.get(start).get(i).weight;
 			}
-
 		}
-		System.out.print(dp[D]);
+	}
+
+	static class Node{
+		int node;
+		int weight;
+
+		Node(int node, int weight) {
+			this.node = node;
+			this.weight = weight;
+		}
 	}
 }
