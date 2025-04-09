a6be4df9c505d747d4b016d4ea8bcad1ca118c1e
dpfprtus <dpfprtus@gmail.com>
2025-04-09 11:11:22 +0900

    정답(다시품)

--- src/백준_문제집/가운데를말해요.java
+++ src/백준_문제집/가운데를말해요.java
@@ -1,35 +1,34 @@
 package 백준_문제집;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.*;
 import java.util.*;
 
 public class 가운데를말해요 {
-    public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        Queue<Integer> minHeap = new PriorityQueue<>();
+		Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+		Queue<Integer> minHeap = new PriorityQueue<>();
 
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (maxHeap.size() == minHeap.size()) {
-                maxHeap.offer(num);
-            }else{
-                minHeap.offer(num);
-            }
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(maxHeap.size() == minHeap.size()){
+				maxHeap.offer(num);
+			} else{
+				minHeap.offer(num);
+			}
 
-            if (!maxHeap.isEmpty() && !minHeap.isEmpty()) {
-                if (maxHeap.peek() > minHeap.peek()) {
-                    int tmp = maxHeap.poll();
-                    maxHeap.offer(minHeap.poll());
-                    minHeap.offer(tmp);
-                }
-            }
-            System.out.println(maxHeap.peek());
-        }
-    }
+			if(!maxHeap.isEmpty() && !minHeap.isEmpty()){
+				if (maxHeap.peek() > minHeap.peek()) {
+					int tmp = minHeap.poll();
+					minHeap.offer(maxHeap.poll());
+					maxHeap.offer(tmp);
+				}
+			}
+			System.out.println(maxHeap.peek());
+		}
+	}
+
 }
